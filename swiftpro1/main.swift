a456b4bdb0f4b7ed124b51880de29115a87bb60f
Athira Paul <athira.paul24@gmail.com>
2019-02-07 19:56:40 -0500

    Initial Commit

---
+++ swiftpro1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swiftpro1
+//
+//  Created by Athira Paul on 2019-02-07.
+//  Copyright © 2019 Athira Paul. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
